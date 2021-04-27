33352097eee15e5419f796f51b7615a191414e90
mdewayne <sawden@mail.ru>
2021-04-27 23:16:57 +0300

    all works!!!! 23 15 27 2021

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -2,26 +2,21 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	**new;
+	t_list	*new;
 	t_list	*temp;
-	void	*newcon;
 
-	temp = NULL;
-	newcon = NULL;
-	if (lst)
-		temp = ft_lstnew(f(lst->content));
-	new = &temp;
-	while (lst->next)
+	new = ft_lstnew(f(lst->content));
+	temp = new;
+	while(lst->next && new)
 	{
+		temp->next = ft_lstnew(f(lst->next->content));
 		lst = lst->next;
-		newcon = f(lst->content);
-		temp->next = ft_lstnew(newcon);
 		if (temp->next == NULL)
 		{
-			ft_lstclear(new, del);
+			ft_lstclear(&new, del);
 			return (NULL);
 		}
 		temp = temp->next;
 	}
-	return(*new);
+	return(new);
 }
